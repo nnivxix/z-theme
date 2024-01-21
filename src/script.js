8247480627cebd1b30805387be5ba4dd6e609cd1
nnivxix <adhe.hanasa@gmail.com>
2024-01-21 21:53:20 +0700

    feat: navbar on mobile

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
 console.log("hello wp");
+const toggleMenu = document.getElementById("toggle-menu");
+const menuMobile = document.getElementById("menu-mobile");
+
+toggleMenu.addEventListener("click", function () {
+  console.log("toggle fired");
+
+  menuMobile.classList.toggle("hidden");
+});
--- template-parts/template-navbar.php
+++ template-parts/template-navbar.php
@@ -1,10 +1,24 @@
-<navbar class="flex max-w-7xl mx-auto justify-between items-center my-2 px-3 font-serif border-b-2 pb-1">
+<navbar class="flex max-w-7xl mx-auto justify-between items-center my-2 px-3 font-serif border-b-2 pb-1 relative">
   <a href="<?php echo home_url() ?>">
     <h1 class="text-2xl font-semibold"> <?php echo get_bloginfo('name'); ?> </h1>
   </a>
-  <button class="p-3 block md:hidden ">
+  <button id="toggle-menu" class="p-3 block md:hidden ">
     &#9776;
   </button>
+  <?php
+  wp_nav_menu(
+    array(
+      'menu' => 'primary',
+      'menu_class' => 'menu-mobile hidden',
+      'theme_location' => 'primary',
+      'menu_id' => 'menu-mobile',
+      'container' => 'div',
+      'items_wrap' => '<ul id="" class="flex text-sm-center text-md-left">%3$s</ul>',
+      // 'items_wrap'           => '<ul style="display:flex;"  class="nav your_custom_class">%3$s</ul>',
+    )
+  )
+  ?>
+
   <?php
   wp_nav_menu(
     array(
